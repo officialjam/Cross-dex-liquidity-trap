9067967eef502b9bb16dcd937b48a21a183e5c2b
0xJRD <84913673+officialjam@users.noreply.github.com>
2025-10-01 23:29:48 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true, // ✅ fixes "stack too deep"
+    },
+  },
   networks: {
-    hardhat: {}
-  }
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
 };
